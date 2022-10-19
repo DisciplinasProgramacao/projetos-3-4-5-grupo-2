104d85c1de5a49af8863b9a2ff26fad8a11bcada
MatheusMOAndrade <andradematheusmo@gmail.com>
2022-10-19 02:37:25 -0300

    localizar veiculo

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -1,4 +1,23 @@
+import java.util.LinkedList;
+
 public class App {
+
+    private LinkedList<Veiculo> listaVeiculos = new LinkedList<Veiculo>();
+
+    public Veiculo localizarVeiculo(String placa) {
+        
+        Veiculo veiculoProcurado = null;
+
+        for (int i = 0; i < listaVeiculos.size(); i++) {   
+            if(listaVeiculos.get(i).placa.equals(placa)) {
+                veiculoProcurado = listaVeiculos.get(i);  
+            }
+        }
+
+        return veiculoProcurado;
+    }    
+
+
     public static void main(String[] args) throws Exception {
         
         
