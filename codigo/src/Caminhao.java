89dfdd4104cf9f607fa6401c13ee7aff825b2355
SamaraMF00 <samara.ferreia.1328767@sga.pucminas.br>
2022-11-16 21:27:16 -0300

    Revert "alteracoes"
    This reverts commit e0d335b5bb2df84729da9f0c4895bee95ab4765c.
    
    # Conflicts:
    #	codigo/src/CustoFixo.java
    #	codigo/src/ICusto.java
    #	codigo/src/TControleKm.java
    #	codigo/src/TVeiculo.java
    #	codigo/src/Veiculo.java
    #	codigo/src/VeiculoTest.java

---
+++ codigo/src/Caminhao.java
@@ -0,0 +1,68 @@
+import javax.print.attribute.standard.RequestingUserName;
+public class Caminhao extends Veiculo{
+
+    //#region - Atributos
+    private static final double TANQUE_MAX = 250;    
+    private static final double IPVA = 0.01;
+    private static final double SEGURO_TAXA = 0.02;
+    private static final double SEGURO_FIXO = 2000;
+    private static final double MANUTENCAO_VALOR = 1000;    
+    private static final double VISTORIA_VALOR = 1000;
+    //#endregion
+
+    //#region - Construtores
+    /**
+     * Construtor caminhão
+     * @param linha Linha de dados proveniente do arquivo.
+     */
+    public Caminhao(String linha) {
+        super(linha);
+    }
+    //#endregion
+
+    //#region - Métodos
+
+    /**
+     * Método para calcular o IPVA do caminhão chamando o método da classe veículo
+     */
+    public double calcularIPVA(){
+        return super.calcularIPVA(IPVA);
+    };
+
+    //#region - Métodos
+    
+    /**
+     * Método para calcular o seguro do caminhão chamando o método da classe veículo
+     */
+    public double calcularSeguro() {
+        return super.calcularSeguro(SEGURO_TAXA, SEGURO_FIXO);
+    };
+
+    /**
+     * Método para calcular os outros custos do caminhão chamando o método da classe veículo
+     */
+    protected double calcularOutrosCustos() {
+        return calcularManutencao() + calcularVistoria();
+    }
+
+    /**
+     * Método para calcular custos de manutenção do caminhão
+     */
+    private double calcularManutencao() {
+        return (kmRodados/20000.0) * MANUTENCAO_VALOR;
+    };
+
+       /**
+     * Método para calcular custos de vistoria do caminhão
+     */
+    private double calcularVistoria() {
+        return (kmRodados/30000.0) * VISTORIA_VALOR;
+    };
+
+    @Override
+    public String toString() {
+        return String.format("%s;", Caminhao.class.getSimpleName())+super.toString();
+    }
+    //#endregion
+
+}
