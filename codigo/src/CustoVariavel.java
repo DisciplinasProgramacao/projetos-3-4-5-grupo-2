a45d08a62f363068c5fe45a5517586ecab37afdc
Paganini <paganini.xxi@gmail.com>
2022-11-25 01:36:46 -0300

    "Documentation"

--- codigo/src/CustoVariavel.java
+++ codigo/src/CustoVariavel.java
@@ -2,11 +2,20 @@ public class CustoVariavel {
     private double valor;
     private String descricao;
 
+    /**
+     * Construtor da classe
+     * @param valor valor do custo variável
+     * @param descricao descrição do custo variável
+     */
     public CustoVariavel(double valor, String descricao) {
         this.valor = valor;
         this.descricao = descricao;
     }
 
+    /**
+     * Método retorna o custo variável 
+     * @return
+     */
     public double retornaCustoVariavel() {
         return valor;
     }
